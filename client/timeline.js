de6f7a58688ce44e94954735ecbb59892b824bad
Dylan Thomson <dylan@storyvision-games.com>
2015-03-22 22:20:56 +0100

    fixed object stuff

--- client/timeline.js
+++ client/timeline.js
@@ -1,29 +1,30 @@
-var TimelineService = function(){
-	return {
-		timeline: null,
+var TimelineService = new function() {
 
-		currentDate: new Date(),
+	this.timeline = null;
 
-		options: {
-			width: "100%",
-			height: '30px',
-			zoomMax: 31536000000, // 1 year
-			zoomMin: 3600000, // 1 day
-			showCurrentTime: false,
-			end: this.currentDate.getTime(),
-			start: this.currentDate.getTime() - 86400000 //default: current time minus 1day
-		},
+	this.currentDate = new Date();
 
-		getFilter: function() {
-			return {
-				created_at: {
-					$gte: Date(this.options.start).toISOString(),
-					$lt: Date(this.options.end).toISOString()
-				}
-			};
-		}
+	this.options = {
+		width: "100%",
+		height: '150px',
+		zoomMax: 31536000000, // 1 year
+		zoomMin: 60000, // 1 day
+		max: this.currentDate,
+		min: new Date(2015, 22, 03),
+		showCurrentTime: false,
+		end: this.currentDate.getTime(),
+		start: this.currentDate.getTime() - 86400000 //default: current time minus 1day
+	};
+
+	this.getFilter = function() {
+		return {
+			created_at: {
+				$gte: Date(this.options.start).toISOString(),
+				$lt: Date(this.options.end).toISOString()
+			}
+		};
 	}
-};
+}();
 
 Template.timeline.helpers({
 	events: function () {
