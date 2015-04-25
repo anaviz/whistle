5fbe755219482409f720d1c108dd533e436bfec1
Ana Vizcarrondo <ana.vizcarrondo@gmail.com>
2015-04-25 19:59:48 +0200

    Added windowRange methods

--- client/lib/events-service.js
+++ client/lib/events-service.js
@@ -1,25 +1,45 @@
-function EventService() {
-	return {
-		getEvents: function (value) {
-			//var filter = value ? value : this.getFilter();
-			//TODO: use filter
-			return Events.find({},
-				{sort: {createdAt: -1}});
-		},
+Meteor.eventService = new function EventService() {
+	this.windowRange = {};
+	this.location = {};
+	this.windowRangeChanged = new Meteor.event();
 
-		getFilter: function() {
-			return {
-				//TODO: add location filter etc
-				created_at: {
-					$gte: new Date(this.currentDate.getTime - 86400000).toISOString(),
-					$lt: this.currentDate.toISOString()
-				}
-			};
-		},
+	this.getEvents = function (value) {
+		//var filter = value ? value : this.getFilter();
+		//TODO: use filter
+		return Events.find({},
+			{sort: {createdAt: -1}});
+	};
 
-		setLocation: function() {
+	this.getFilter = function() {
+		return {
+			//TODO: add location filter etc
+			created_at: {
+				$gte: new Date(this.currentDate.getTime - 86400000).toISOString(),
+				$lt: this.currentDate.toISOString()
+			}
+		};
+	};
 
+	this.setLocation = function(location) {
+
+	};
+
+	this.setWindowRange = function(windowRange) {
+		if(this.windowRange !== windowRange) {
+			this.windowRange = windowRange;
+			this.windowRangeChanged.trigger();
 		}
-	}
+	};
+
+	this.getWindowRange = function(windowRange) {
+		return this.windowRange;
+	};
+
+	this.getWindowRangeStart = function(windowRange) {
+		return this.windowRange.start;
+	};
+
+	this.getWindowRangeEnd = function(windowRange) {
+		return this.windowRange.end;
+	};
 };
-Meteor.eventService = new EventService();
--- client/timeline.js
+++ client/timeline.js
@@ -1,36 +1,36 @@
-function TimelineController() {
-	return {
-		timeline: null,
+var timelineController = new function() {
+	this.eventService = Meteor.eventService;
 
-		currentDate: new Date(),
-
-		options: {
-			width: "100%",
-			height: '150px',
-			zoomMax: 31536000000, // 1 year
-			zoomMin: 60000, // 1 minute
-			max: new Date().getTime(), // now
-			min: new Date(1420066800000), //01/01/2015
-			showCurrentTime: false
-			//end: this.currentDate.getTime(), //end of the events shown
-			//start: this.currentDate.getTime() - 86400000 //start of the events shown //default: current time minus 1day
-		},
-
-		createTimeline: function (container) {
-			var items = Meteor.eventService.getEvents().fetch();
-			this.timeline = new vis.Timeline(container, items, this.options);
-			this.timeline.on('rangechanged', this.timelineOnRangechanged.bind(this));
-		},
-
-		timelineOnRangechanged: function (range) {
-			console.log("update map");
-			// TODO: TRIGGER CHANGE
-		}
+	this.timelineOptions = {
+		width: "100%",
+		height: '150px',
+		zoomMax: 31536000000, // 1 year
+		zoomMin: 60000, // 1 minute
+		max: new Date().getTime(), // now
+		min: new Date(1420066800000), // 01/01/2015
+		showCurrentTime: false,
+		end: Meteor.eventService.getWindowRangeEnd(), // WindowRange. If undefined the end will be the last of the events passed
+		start: Meteor.eventService.getWindowRangeStart() // WindowRange. If undefined the start will be the first of the events passed
 	};
-};
-var TimelineController = new TimelineController();
+
+	this.createTimeline = function (container) {
+		var items = this.eventService.getEvents().fetch();
+		this.timelineObject = new vis.Timeline(container, items, this.timelineOptions);
+		this.timelineObject.on('rangechanged', this.timelineOnWindowRangeChanged.bind(this));
+	};
+
+	this.setTimelineRange = function (windowRange) {
+		this.timelineObject.setWindow(windowRange.start, windowRange.end);
+	};
+
+	this.timelineOnWindowRangeChanged =  function (range) {
+		this.eventService.setWindowRange(range);
+	};
+
+	this.eventService.windowRangeChanged.add(this.setTimelineRange, this);
+}();
 
 Template.timeline.rendered = function() {
 	var timeNavigator = this.find("#time-navigator");
-	TimelineController.createTimeline(timeNavigator);
+	timelineController.createTimeline(timeNavigator);
 };
