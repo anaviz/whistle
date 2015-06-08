5b64bcb9b613bed83e2e686644e9370931b5c0ce
Dylan Thomson <dylan@storyvision-games.com>
2015-06-08 11:51:48 +0200

    implemented react views and require

--- client/main.js
+++ client/main.js
@@ -1,17 +1,40 @@
-Meteor.subscribe("events");
-Meteor.subscribe("userData");
+define("main",
+	["timelineController", "views/react/test"],
+	function(TimelineController, testView) {
+		Meteor.subscribe("events");
+		Meteor.subscribe("userData");
 
-Template.body.helpers({
-	events: function () {
-		return Events.find({}, {sort: {createdAt: -1}});
+		Template.body.helpers({
+			events: function () {
+				return Events.find({}, {sort: {createdAt: -1}});
+			}
+
+			// Use Session to store just-one-time-session info
+			//someName: function () {
+			//	return Session.get("someName");
+			//}
+		});
+
+		Accounts.ui.config({
+			passwordSignupFields: "USERNAME_ONLY"
+		});
+
+		// Initialize timeline controller
+		var timelineController = new TimelineController();
+		var timeNavigatorContainer = document.getElementById("time-navigator");
+		timelineController.createTimeline(timeNavigatorContainer);
+
+		//new testView(timeNavigatorContainer);
 	}
+);
 
-	// Use Session to store just-one-time-session info
-	//someName: function () {
-	//	return Session.get("someName");
-	//}
-});
+Meteor.startup(function(){
+	// Shim vis library into require. Must inject "vis" as dep into module definition
+	require.config({
+		paths: {
+			vis: 'client/requireLibraries/vis'
+		}
+	});
 
-Accounts.ui.config({
-	passwordSignupFields: "USERNAME_ONLY"
+	require(["main"]);
 });
